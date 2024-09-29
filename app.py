94b92ea9495aec816c6d34392484a50c2a9951b1
Jacky Le <jacky.le408@gmail.com>
2024-09-29 03:44:17 -0400

    finished layer buttons

--- app.py
+++ app.py
@@ -7,77 +7,106 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    # initialize map, no tiles
+    # Initialize map without tiles
     m = folium.Map(location=[39.255535, -76.711329],
-                    zoom_start=15.5, tiles=None)
-    # add light map tile (default)
-    folium.TileLayer(tiles="OpenStreetMap", name="Day Mode").add_to(m)
-    # add dark map tile
-    folium.TileLayer(tiles="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png",
-           attr="<a href=https://docs.stadiamaps.com/map-styles/alidade-smooth-dark/>Alidade Smooth Dark</a>", name="Night Mode", show=False).add_to(m)
+                   zoom_start=15.5, tiles=None)
+    
+    # Add map tiles and store them in variables
+    day_tile = folium.TileLayer(tiles="OpenStreetMap", name="Day Mode").add_to(m)
+    night_tile = folium.TileLayer(
+        tiles="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png",
+        attr="<a href=https://docs.stadiamaps.com/map-styles/alidade-smooth-dark/>Alidade Smooth Dark</a>",
+        name="Night Mode", show=False
+    ).add_to(m)
 
-    # adds lamps layer
-    # remove markers for now
-    # lamps = folium.FeatureGroup(name="Lamp Locations", show=False).add_to(m)
+    # Add layers and get their names
     lamp_coords = []
     gradient = {.33: 'yellow', .66: 'orange', 1: 'white'}
-    with open("lamps.csv", mode = "r") as file:
+    with open("lamps.csv", mode="r") as file:
         csvFile = csv.reader(file)
         for line in csvFile:
             lamp_coords.append([float(line[1]), float(line[2])])
-            # folium.Marker([line[1], line[2]], popup="lamp", icon=folium.Icon(color="beige",
-                        # icon="fa-solid fa-lightbulb",prefix="fa")).add_to(lamps)
-    HeatMap(lamp_coords, name="Lamps", min_opacity=3, gradient=gradient, radius=7, show=False).add_to(m)
-
-    # add sos phones to emergency resource layer
-    sos = folium.FeatureGroup(name="Emergency Resources", show=False).add_to(m)
-    with open("sos.csv", mode = "r") as file:
+    lamp_layer = HeatMap(lamp_coords, name="Lamps", min_opacity=3,
+                         gradient=gradient, radius=7, show=False).add_to(m)
+    
+    sos_layer = folium.FeatureGroup(name="Emergency Resources", show=False).add_to(m)
+    with open("sos.csv", mode="r") as file:
         csvFile = csv.reader(file)
         for line in csvFile:
             folium.Marker([line[1], line[2]], popup="SOS Phone",
-                          icon=folium.Icon(color="blue",icon="fa-regular fa-bell",prefix="fa")).add_to(sos)
-    # add police station to emergency resource layer
-    folium.Marker([39.25984373757963, -76.71630620628858], popup="Baltimore County Police Department",
-                  icon=folium.Icon(color="darkblue",icon="fa-solid fa-shield-dog",prefix="fa")).add_to(sos)
-    folium.Marker([39.25727885338491, -76.7141845789751], popup="UMBC Campus Police Department",
-                  icon=folium.Icon(color="darkblue",icon="fa-solid fa-shield-dog",prefix="fa")).add_to(sos)
+                          icon=folium.Icon(color="blue",
+                          icon="fa-regular fa-bell", prefix="fa")).add_to(sos_layer)
+    folium.Marker([39.25984373757963, -76.71630620628858],
+                  popup="Baltimore County Police Department",
+                  icon=folium.Icon(color="darkblue",
+                  icon="fa-solid fa-shield-dog", prefix="fa")).add_to(sos_layer)
+    folium.Marker([39.25727885338491, -76.7141845789751],
+                  popup="UMBC Campus Police Department",
+                  icon=folium.Icon(color="darkblue",
+                  icon="fa-solid fa-shield-dog", prefix="fa")).add_to(sos_layer)
 
-
-    # add construction closures layer
-    construction = folium.FeatureGroup(name="Construction Closures", show=False).add_to(m)
-    with open("construction.csv", mode = "r") as file:
+    construction_layer = folium.FeatureGroup(name="Construction Closures", show=False).add_to(m)
+    with open("construction.csv", mode="r") as file:
         csvFile = csv.reader(file)
         for line in csvFile:
             folium.Marker([line[1], line[2]], popup="Closed for construction",
-                          icon=folium.Icon(color="red",icon="fa-solid fa-xmark",prefix="fa")).add_to(construction)
+                          icon=folium.Icon(color="red",
+                          icon="fa-solid fa-xmark", prefix="fa")).add_to(construction_layer)
 
-    # add lactation rooms layer
-    lactation = folium.FeatureGroup(name="Lactation Rooms", show=False).add_to(m)
-    with open("lactation_rooms.csv", mode = "r") as file:
+    lactation_layer = folium.FeatureGroup(name="Lactation Rooms", show=False).add_to(m)
+    with open("lactation_rooms.csv", mode="r") as file:
         csvFile = csv.reader(file)
         for line in csvFile:
-            folium.Marker([line[1], line[2]], popup=line[0], tooltip="Click for the location in this building",
-                          icon=folium.Icon(color="lightred",icon="fa-solid fa-person-breastfeeding",prefix="fa")).add_to(lactation)
+            folium.Marker([line[1], line[2]], popup=line[0],
+                          tooltip="Click for the location in this building",
+                          icon=folium.Icon(color="lightred",
+                          icon="fa-solid fa-person-breastfeeding", prefix="fa")).add_to(lactation_layer)
 
-    # add family restrooms layer
-    family_restrooms = folium.FeatureGroup(name="Family Friendly Restrooms", show=False).add_to(m)
-    with open("family_restrooms.csv", mode = "r") as file:
+    family_restrooms_layer = folium.FeatureGroup(name="Family Friendly Restrooms", show=False).add_to(m)
+    with open("family_restrooms.csv", mode="r") as file:
         csvFile = csv.reader(file)
         for line in csvFile:
-            folium.Marker([line[1], line[2]], popup=line[0], tooltip="Click for locations inside this building",
-                          icon=folium.Icon(color="lightblue",icon="fa-solid fa-baby",prefix="fa")).add_to(family_restrooms)
+            folium.Marker([line[1], line[2]], popup=line[0],
+                          tooltip="Click for locations inside this building",
+                          icon=folium.Icon(color="lightblue",
+                          icon="fa-solid fa-baby", prefix="fa")).add_to(family_restrooms_layer)
 
-    # adds layer controls to map
-    folium.LayerControl().add_to(m)
+    # Remove built-in LayerControl
+    # folium.LayerControl().add_to(m)
 
+    # Get layer and map names
+    lamp_layer_name = lamp_layer.get_name()
+    sos_layer_name = sos_layer.get_name()
+    construction_layer_name = construction_layer.get_name()
+    lactation_layer_name = lactation_layer.get_name()
+    family_restrooms_layer_name = family_restrooms_layer.get_name()
+    map_name = m.get_name()
+
+    # Get tile layer names
+    day_tile_name = day_tile.get_name()
+    night_tile_name = night_tile.get_name()
+
+    # Save and render the map
     m.save("map.html")
-    # convert folium map to html
     m.get_root().render()
     header = m.get_root().header.render()
     body_html = m.get_root().html.render()
     script = m.get_root().script.render()
     
-    return render_template("index.html", header=header, body_html=body_html, script=script)
+    return render_template(
+        "index.html",
+        header=header,
+        body_html=body_html,
+        script=script,
+        lamp_layer_name=lamp_layer_name,
+        sos_layer_name=sos_layer_name,
+        construction_layer_name=construction_layer_name,
+        lactation_layer_name=lactation_layer_name,
+        family_restrooms_layer_name=family_restrooms_layer_name,
+        day_tile_name=day_tile_name,
+        night_tile_name=night_tile_name,
+        map_name=map_name
+    )
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port = 5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
